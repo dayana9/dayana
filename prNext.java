d86e965b431dc2f9890d7cde398aac3c00549e4e
dayana9 <edayana173@gmail.com>
2016-09-11 14:07:23 +0530

    Add files via upload

---
+++ prNext.java
@@ -0,0 +1,42 @@
+
+
+import java.io.*; 
+
+class prNext 
+{
+
+            public static void main(String Ar[]) 
+           {
+
+               int a=0,i,j; 
+               DataInputStream ins=new DataInputStream(System.in); 
+              try 
+               {
+
+                     System.out.println(“Enter the no.:”); 
+                     a=Integer.parseInt(ins.readLine()); 
+                } 
+                    catch(IOException e) 
+                    {} 
+                    for(j=a+1;;j++) 
+                    {
+
+                         for(i=2;i<j;i++) 
+                         { 
+
+                             if(j%i==0) 
+                             break; 
+                         } 
+                      
+
+                      if  (i==j) 
+                     {
+
+                        System.out.println(“Next prime no is:”+j); 
+                        break; 
+                    }
+
+              }
+
+       } 
+}
